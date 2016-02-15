66a8319cce9575d2af46e30013e6d1e874959ba2
CodyBurton <5887963@gmail.com>
2016-02-15 02:16:42 +0000

    a3 site update

--- htdocs/a3/index.php
+++ htdocs/a3/index.php
@@ -19,6 +19,7 @@ ob_start("sanitize_output");
 <!DOCTYPE html>
 <html>
 	<head>
+		<title>IGAIR - ARMA 3</title>
 		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -34,8 +35,8 @@ ob_start("sanitize_output");
 			<div class="col s12">
 				<ul class="tabs">
 					<li class="waves-effect waves-light tab col s3"><a class="active" href="#tab1">INTRODUCTION</a></li>
-					<li class="waves-effect waves-light tab col s3"><a href="#tab3">MODPACK INFORMATION</a></li>
-					<li class="waves-effect waves-light tab col s3 disabled"><a href="#tab4">MEDIA (COMING SOON)</a></li>
+					<li class="waves-effect waves-light tab col s3"><a href="#tab2">MODPACK INFORMATION</a></li>
+					<li class="waves-effect waves-light tab col s3 disabled"><a href="#tab3">MEDIA (COMING SOON)</a></li>
 				</ul>
 			</div>
 			<br><br>
@@ -57,57 +58,56 @@ ob_start("sanitize_output");
 					to have more players attend!
 				</div>
 			</div>
-			<div id="tab3" class="col s12 center">
+			<div id="tab2" class="row center">
 				<i class="medium material-icons">cloud_download</i>
 				<br><br>
-				<ul class="collapsible" data-collapsible="accordion">
-					<li>
-						<div class="collapsible-header red white-text lighten-1">INFORMATION</div>
-						<div class="collapsible-body">
-							<h4>Current Mods List:</h4>
-							<?php
-							$dir = 'C:\xampp\htdocs\a3\mods';
-							$scanArray = scandir('C:\xampp\htdocs\a3\mods');
-							unset($scanArray[0]);
-							unset($scanArray[1]);
-							unset($scanArray[2]);
-							$trunc = implode('<br>', $scanArray);
-							echo($trunc);
-							$countArray = count($scanArray);
-							echo("<br><br> <font color='blue'><b>Total Mods: </b></font>");
-							echo($countArray);
-							$sizeArray = disk_total_space($dir);
-							echo("<br><font color='blue'><b>Total Size: </b></font>");
-							$cutStr = substr($sizeArray, 0, 2);
-							echo($cutStr);echo("GB");
-							?>
-							<br><br>
-						</div>
-					</li>
-					<li>
-						<div class="collapsible-header red white-text lighten-1">INSTALLATION</div>
-						<div class="collapsible-body"><br>
-							<ol>
-								<li>Download the <a href="http://www.armaholic.com/page.php?id=22199" target="_blank">Arma3Sync Client</a></li>
-								<li>Point the client to your Arma 3 directory</li>
-								<li>Create a new directory to contain your mods for IGAIR and add it to your "Addon Search Directories"</li>
-								<li>Add a new repository using this import link: <font color="red">http://igair.ddns.net/a3/mods/.a3s/autoconfig</font></li>
-								<li>Connect to the repository and use the "exact match" option when checking the mods</li>
-								<li>Use the new "join server" entry after downloading</li>
-							</ol>
-							<br>
-							<iframe width="560" height="315" src="https://www.youtube.com/embed/XmxKN8DVeyc" frameborder="0" allowfullscreen></iframe>
-							<br><br>
-						</div>
-					</li>
-				</ul>
+				<div class="col s12 blue white-text">
+					<h4><font color='yellow'><b>Current Mods List:</b></font></h4>
+					<?php
+						$dir = 'C:\xampp\htdocs\a3\mods';
+						$scanArray = scandir('C:\xampp\htdocs\a3\mods');
+						unset($scanArray[0]);
+						unset($scanArray[1]);
+						unset($scanArray[2]);
+						$implode = implode(', ', $scanArray);
+						$trunc = preg_replace('/@/', '', $implode);
+						echo($trunc);
+						$countArray = count($scanArray);
+						echo("<br><br> <font color='yellow'><b>Total Mods: </b></font>");
+						echo($countArray);
+						$sizeArray = disk_total_space($dir);
+						echo("<br><font color='yellow'><b>Total Size: </b></font>");
+						$cutStr = substr($sizeArray, 0, 2);
+						echo($cutStr);echo("GB");
+					?>
+					<br><br>
+				</div>
+				<div class="col s12 yellow">
+					<h4><font color='red'><b>Installation:</b></font></h4>
+					<ol class="left-align">
+						<li>Download and install the <a href="http://www.armaholic.com/page.php?id=22199" target="_blank">Arma3Sync Client</a></li>
+						<li>Point the client to your Arma 3 directory</li>
+						<li>Create a new directory to contain your mods for IGAIR and add it to your "Addon Search Directories"</li>
+						<li>Add a new repository using this import link: <font color="red">http://igair.ddns.net/a3/mods/.a3s/autoconfig</font></li>
+						<li>Connect to the repository and use the "exact match" option when checking the mods</li>
+						<li>Use the new "join server" entry after downloading</li>
+					</ol>
+					<br>
+				</div>
+				<div class="col s6 red">
+					<h4><font color='white'><b>YouTube Video:</b></font></h4>
+					<div class="video-container">
+						<iframe src="//www.youtube.com/embed/nTiINV-3IL8?rel=0" frameborder="0" allowfullscreen></iframe>
+					</div>
+					<br>
+				</div>
 			</div>
-			<div id="tab4" class="col s12 center">
+			<div id="tab3" class="col s12 center">
 				<i class="medium material-icons">play_circle_outline</i>
 				WHAT YOU LOOKING AT?
 			</div>
 		</div>
-		<div id="modal1" class="modal bottom-sheet">
+		<div id="modalCookie" class="modal bottom-sheet">
 			<div class="modal-content">
 				<img src="res/codyCookie.png"/><h4>Cookies ahead!</h4>
 				<p>By continuing to use/browse this site you are agreeing to the potential use of cookies.<br><br>
@@ -117,7 +117,7 @@ ob_start("sanitize_output");
 		</div>
 		<?php
 		if(!isset($_COOKIE['codyCookie'])) {
-			echo("<script>$('#modal1').openModal();</script>");
+			echo("<script>$('#modalCookie').openModal();</script>");
 			setcookie('codyCookie', 'codyWasHere');
 		}
 		?>
